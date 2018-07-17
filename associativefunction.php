72dac145be3bbd15abb5ff565fd2d9e3167afffc
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-17 08:29:52 -0600

    Added age array and populated it with 10 entries.

---
+++ associativefunction.php
@@ -0,0 +1,12 @@
+<?php
+$age=array("Tom"=>"30", "Rick"=>"45", "Mary"=>"55", "Shelly"=>"42","Mark"=>"21", "Adam"=>"18", "Sue"=>"41", "Alan"=>"55", "Daniel"=>"25", "Paula"=>"40");
+echo "Tom is " . $age['Tom'] . " years old.<br>";
+echo "Rick is " . $age['Rick'] . " yesrs old.<br>";
+echo "Mary is " . $age['Mary'] . " years old.<br>";
+echo "Shelly is " . $age['Shelly'] . " years old.<br>";
+echo "Mark is " . $age['Mark'] . " years old.<br>";
+echo "Adam is " . $age['Adam'] . " years old.<br>";
+echo "Sue is " . $age['Sue'] . " years old.<br>";
+echo "Alan is " . $age['Alan'] . " years old.<br>";
+echo "Daniel is " . $age['Daniel'] . " years old.<br>";
+echo "Paula is " . $age['Paula'] ." years old.<br>";
