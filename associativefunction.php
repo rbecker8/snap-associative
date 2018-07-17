7704caef826ea5eeb68adb7b40407f7184bd84c3
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-17 08:30:38 -0600

    Corrected spelling error in entry 2.

--- associativefunction.php
+++ associativefunction.php
@@ -1,7 +1,7 @@
 <?php
 $age=array("Tom"=>"30", "Rick"=>"45", "Mary"=>"55", "Shelly"=>"42","Mark"=>"21", "Adam"=>"18", "Sue"=>"41", "Alan"=>"55", "Daniel"=>"25", "Paula"=>"40");
 echo "Tom is " . $age['Tom'] . " years old.<br>";
-echo "Rick is " . $age['Rick'] . " yesrs old.<br>";
+echo "Rick is " . $age['Rick'] . " years old.<br>";
 echo "Mary is " . $age['Mary'] . " years old.<br>";
 echo "Shelly is " . $age['Shelly'] . " years old.<br>";
 echo "Mark is " . $age['Mark'] . " years old.<br>";
